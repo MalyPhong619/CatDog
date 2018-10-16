d699fd1b0fc2bbc6edb02ddfb637fdd2b43f84ad
christopher cahill and maly phongsavanh <christopher@>
2018-10-16 15:56:16 -0700

    adds this remove to hello button to remove items from cat and dog lists

--- js/scripts.js
+++ js/scripts.js
@@ -2,24 +2,27 @@ $(document).ready(function() {
   $("button#hello").click(function() {
     $("ul#cat").append("<li>hello</li>");
     $("ul#dog").append("<li>Hey whats up cat</li>");
-    $("ul").children("li").click(function() {
+    $("ul#cat").children("li").click(function() {
+      $(this).remove();
+    });
+    $("ul#dog").children("li").click(function() {
       $(this).remove();
     });
   });
 
-  $("button#goodbye").click(function() {
-    $("ul#cat").append("<li>goodbye</li>");
-    $("ul#dog").append("<li>Peace Dude</li>");
-    $("ul").children("li").click(function() {
-      $(this).remove();
-    });
-  });
-
-  $("button#stop").click(function() {
-    $("ul#cat").append("<li>stop</li>");
-    $("ul#dog").append("<li>No, you stop!</li>");
-    $("ul").children("li").click(function() {
-      $(this).remove();
-    });
-  });
+  // $("button#goodbye").click(function() {
+  //   $("ul#cat").append("<li>goodbye</li>");
+  //   $("ul#dog").prepend("<li>Peace Dude</li>");
+  //   $("ul#dog").children("li").last().click(function() {
+  //     $(this).remove();
+  //   });
+  // });
+  //
+  // $("button#stop").click(function() {
+  //   $("ul#cat").append("<li>stop</li>");
+  //   $("ul#dog").append("<li>No, you stop!</li>");
+  //   $("ul").children("li").click(function() {
+  //     $(this).remove();
+  //   });
+  // });
 });
